a5115a4190f54bfaf8971665992965f53a96eab5
Ciel Yang <cielsk43@gmail.com>
2021-12-01 15:16:19 +0800

    Solve day 01 puzzle.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var prev = Int.MAX_VALUE
+        var cnt = 0
+        for (line in input) {
+            val curr = line.toInt()
+            if (curr > prev) cnt++
+            prev = curr
+        }
+        return cnt
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        if (input.size < 3) return 0
+        val cache = input.map { it.toInt() }
+        var cnt = 0
+        for (i in 3 until cache.size) {
+            val curr = cache[i]
+            val prev = cache[i - 3]
+            if (curr > prev) cnt++
+        }
+        return cnt
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
