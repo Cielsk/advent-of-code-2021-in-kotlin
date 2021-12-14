a20ad0df87a260ad2dc7ecef76823fb576dbbb55
Ciel Yang <cielsk43@gmail.com>
2021-12-14 16:17:39 +0800

    Refactor Day 14 answer.

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,96 +1,56 @@
 fun main() {
-    class InsertionRules() {
-        private val data = mutableMapOf<Pair<Char, Char>, Char>()
-
-        fun addRule(key: String, insertion: Char) {
-            val pair = Pair(key[0], key[1])
-            data[pair] = insertion
-        }
-
-        fun getInsertion(pair: Pair<Char, Char>) = data[pair]
-
-        fun getCharSet(): Set<Char> {
-            val set = mutableSetOf<Char>()
-            for ((key, value) in data) {
-                set.add(key.first)
-                set.add(key.second)
-                set.add(value)
-            }
-            return set
-        }
+    class InsertionRules(private val map: Map<String, List<String>>) {
+        fun getInsertResult(key: String) = map[key] ?: emptyList()
     }
 
     fun parseInput(input: List<String>): Pair<String, InsertionRules> {
-        val template = input.takeWhile { it.isNotEmpty() }.first()
-        val insertionRules = InsertionRules()
-        input.takeLastWhile { it.isNotEmpty() }.forEach {
+        val template = input.first()
+        val map = input.drop(2).associate {
             val (key, insertion) = it.split(" -> ")
-            insertionRules.addRule(key, insertion[0])
+            key to listOf("${key[0]}$insertion", "$insertion${key[1]}")
         }
+        val insertionRules = InsertionRules(map)
         return Pair(template, insertionRules)
     }
 
-    fun merge(a: LongArray, b: LongArray, result: LongArray) {
-        for (i in result.indices) {
-            result[i] = a[i] + b[i]
-        }
-    }
-
     fun insert(template: String, insertionRules: InsertionRules, times: Int): List<Long> {
-        val charSet = template.toSet() + insertionRules.getCharSet()
-        val pairList = mutableListOf<Pair<Char, Char>>()
-        for (first in charSet) {
-            for (second in charSet) {
-                pairList.add(Pair(first, second))
-            }
-        }
-        val pairIdxMap = pairList.mapIndexed { index, pair -> Pair(pair, index) }.toMap()
-        val dp = Array(times + 1) { Array(pairList.size) { LongArray(26) } }
-        for (i in pairList.indices) {
-            val pair = pairList[i]
-            dp[0][i][pair.first - 'A']++
-            dp[0][i][pair.second - 'A']++
-        }
-        for (i in 1..times) {
-            for (j in pairList.indices) {
-                val pair = pairList[j]
-                val insertion = insertionRules.getInsertion(pair)
-                if (insertion == null) dp[0][j].copyInto(dp[i][j])
-                else {
-                    val left = Pair(pair.first, insertion)
-                    val right = Pair(insertion, pair.second)
-                    val leftIdx = pairIdxMap[left]!!
-                    val rightIdx = pairIdxMap[right]!!
-                    merge(dp[i - 1][leftIdx], dp[i - 1][rightIdx], dp[i][j])
-                    dp[i][j][insertion - 'A']--
+        var map = template.zip(template.drop(1)) { ch1, ch2 -> "$ch1$ch2" }
+                .groupingBy { it }
+                .eachCount()
+                .mapValues { it.value.toLong() }
+        repeat(times) {
+            map = buildMap {
+                for ((str, cnt) in map) {
+                    for (newStr in insertionRules.getInsertResult(str)) {
+                        compute(newStr) { _, v -> (v ?: 0) + cnt }
+                    }
                 }
             }
         }
         val counter = LongArray(26)
         counter[template[0] - 'A']++
-        for (i in 1 until template.length) {
-            val pair = Pair(template[i - 1], template[i])
-            val idx = pairIdxMap[pair]!!
-            val pairCounter = dp[times][idx]
-            merge(counter, pairCounter, counter)
-            counter[template[i - 1] - 'A']--
+        counter[template.last() - 'A']++
+        for ((str, cnt) in map) {
+            for (c in str) {
+                counter[c - 'A'] += cnt
+            }
         }
-        return counter.filter { it > 0 }
+        return counter.filter { it > 0 }.map { it / 2 }
     }
 
     fun part1(input: List<String>): Int {
         val (template, insertionRules) = parseInput(input)
         val counter = insert(template, insertionRules, 10)
-        val max = counter.maxOrNull() ?: 0L
-        val min = counter.minOrNull() ?: 0L
+        val max = counter.maxOrNull() ?: 0
+        val min = counter.minOrNull() ?: 0
         return (max - min).toInt()
     }
 
     fun part2(input: List<String>): Long {
         val (template, insertionRules) = parseInput(input)
         val counter = insert(template, insertionRules, 40)
-        val max = counter.maxOrNull() ?: 0L
-        val min = counter.minOrNull() ?: 0L
+        val max = counter.maxOrNull() ?: 0
+        val min = counter.minOrNull() ?: 0
         return max - min
     }
 
